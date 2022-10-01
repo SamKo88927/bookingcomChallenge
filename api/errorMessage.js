938887c5dac050b089f1263af8f012d63c2b09a6
SamKo <86094956+SamKo88927@users.noreply.github.com>
2022-10-01 11:35:12 +0800

    Update errorMessage.js

--- api/errorMessage.js
+++ api/errorMessage.js
@@ -3,6 +3,6 @@ export const errorMessage=(status, message,err)=>{
     const orignalErr = err?.message || "條件錯誤"; 
     //怕到時候不一定要傳error變數
     error.status= status;
-    error.message=message+`\n錯誤詳細描述: `+orignalErr;
+    error.message=message+`錯誤詳細描述: `+orignalErr;
     return error; 
 }
