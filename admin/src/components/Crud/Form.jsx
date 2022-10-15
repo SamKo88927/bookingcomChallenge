d6172eb8a764fffcf34f24bf2488272626a197e9
Sam Ko <samko0927@gmail.com>
2022-10-15 16:11:48 +0800

    30 finish admin  10/15 updated commit

--- admin/src/components/Crud/Form.jsx
+++ admin/src/components/Crud/Form.jsx
@@ -7,21 +7,36 @@ import Input from './Input'
 const Form = ({ inputData, backgroundColor, button }) => {
   const [file, setFile] = useState([])
   return (
-    <div className="flex p-5  mt-3 text-white rounded flex-col bg-Crud-hotel bg-left-top bg-cover" style={{ backgroundColor: backgroundColor }} >
+    <div className="flex p-5  mt-3 text-white rounded flex-col  bg-left-top bg-cover" style={{ backgroundColor: backgroundColor }} >
       {inputData.map((item, index) =>
         item.id == "type" || item.id == "city" ?
           <DropdownList key={index} data={item} set={item.set} />
           :
           item.id == "photos" ?
             <div key={index}>
-              <Input  id={item.id} name={item.name} placeholder={item.placeholder} />
-              <input className='my-3' type="file" id="img" accept='image/png,jpeg' onChange={(e)=>setFile([e.target.files])} multiple />
-              {!file ? 
-               <img src={hoteldisplay} width="500px" alt="" />
-              :
-               file.map((item)=>
-               <img src={URL.createObjectURL(item[0])} width="500px" alt="" />
-               )
+              <Input id={item.id} name={item.name} placeholder={item.placeholder} />
+              <input className='my-3 
+              file:bg-white
+              file:text-sky-500
+              hover:file:bg-sky-500
+              hover:file:text-white 
+              file:rounded-lg file:border-0
+              file:p-3' type="file" id="img" accept='image/png,jpeg' onChange={(e) => setFile([e.target.files])} multiple />
+              {!file ?
+                <img src={hoteldisplay} width="500px" alt="" />
+                :
+                file.map((item) =>
+                <a className="group block max-w-full mx-auto p-6 rounded-lg bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500">
+                  <div className="flex items-center gap-5">
+                    <img className="h-48 w-48 rounded-lg" src={URL.createObjectURL(item[0])}  alt="" />
+                    <h3 className="text-slate-900 group-hover:text-white text-sm font-semibold">飯店照片預覽圖
+                    <p className="text-slate-500 group-hover:text-white text-sm">SamkoBooking飯店照片上傳</p>
+                    </h3>
+                  </div>
+               
+                </a>
+                  
+                )
               }
             </div>
             :
@@ -33,7 +48,7 @@ const Form = ({ inputData, backgroundColor, button }) => {
               :
               <Input key={index} id={item.id} name={item.name} placeholder={item.placeholder} />
       )}
-      <button >{button}</button>
+      <button className='mt-4 border-1 py-3 border-white rounded-sm text-white cursor-pointer hover:bg-black'>{button}</button>
     </div>
   )
 }
